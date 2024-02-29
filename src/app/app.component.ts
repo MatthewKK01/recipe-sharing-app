46daddb5bf44e85a963d2d8343deff4a604e4921
MatthewKK01 <mateyavelashvili@gmail.com>
2024-02-29 15:44:20 +0400

    created mock back-end functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'recipe-sharing-app';
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<any>("http://localhost:3000/recipes").subscribe(
+      {
+        next: res => console.log(res),
+        error: err => console.log(err),
+
+      }
+    )
+  }
+  addFn() {
+    const newRecipe = {
+      id: 12,
+      title: "xvanchkara"
+    }
+    this.http.post("http://localhost:3000/recipes", newRecipe, this.httpOptions).subscribe(
+      {
+        next: res => console.log(res)
+      }
+    )
+  }
 }
